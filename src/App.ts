4bbfa4aaac2241a36541eb710ee30027885d1031
Daniel Shim <shimdh@gmail.com>
2017-07-11 18:30:50 +0900

    Added App.ts and index.ts

---
+++ src/App.ts
@@ -0,0 +1,36 @@
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as logger from "morgan";
+import * as path from "path";
+
+class App {
+
+    // ref to Express instance
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    // Configure Express middleware.
+    private middleware(): void {
+        this.express.use(logger("dev"));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private routes(): void {
+        const router = express.Router();
+        router.get("/", (req, res, next) => {
+            res.json({
+                message: "Hello World!",
+            });
+        });
+        this.express.use("/", router);
+    }
+
+}
+
+export default new App().express;
